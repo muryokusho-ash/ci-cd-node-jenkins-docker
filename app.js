e9632c1ff75526452d190878d8dd1a4e302e1868
Ash <muryokusho.ash.k@gmail.com>
2026-05-24 01:13:13 +0530

    add mistake

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ console.log("===========================================")
 console.log("Running app...")
 console.log("===========================================")
 function add(a, b) {
-  return a + b;
+  return a * b;
 }
 
 console.log("Addition Result: ", add(2, 3));
